6ff798d436eb070af68690e2bdda3ac856cd5f4d
aniroy29 <aniroy2937@gmail.com>
2026-08-21 18:19:15 +0600

    js done

--- final.js
+++ final.js
@@ -1,6 +1,9 @@
 var MAX_INITIAL_RESULTS = 5;
 var allData = [];
 
+
+
+
 function connect(){
 
     var search  = document.getElementById("searchTerm").value ;
@@ -20,3 +23,49 @@ function connect(){
 
 }
 
+
+
+function show (data){
+
+  allData = data.meals;
+
+  render(allData.slice(0, MAX_INITIAL_RESULTS));
+
+  var showAllBtn = document.getElementById("showAllBtn");
+  if (allData.length > MAX_INITIAL_RESULTS){
+      showAllBtn.classList.remove("hidden");
+  }
+
+}
+
+
+
+function showAll(){
+
+    var oldContent = document.getElementById("displayArea");
+    oldContent.innerHTML = "";
+
+    render(allData);
+
+    document.getElementById("showAllBtn").classList.add("hidden");
+
+}
+
+
+
+function render(mealsToShow){
+
+  var oldContent = document.getElementById("displayArea");
+
+  for (var i = 1; i <= mealsToShow.length; i++){
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML =`Meal ID: ${mealsToShow[i-1].idMeal} <br>
+                       Meal Name: ${mealsToShow[i-1].strMeal} <br>
+                       Meal Title: ${mealsToShow[i-1].strMeal} <br>
+                       <img src="${mealsToShow[i-1].strMealThumb}" <br>
+                       Cooking Instructions: ${mealsToShow[i-1].strInstructions} <br>`;
+        newDiv.classList.add("innerStyle")
+
+        oldContent.appendChild(newDiv);
+  }
+}
